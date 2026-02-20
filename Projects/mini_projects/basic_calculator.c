16138b7b9397bd758e187c528390a81dcfabe5c0
Johnzel Marahay <224951203+zlxdev@users.noreply.github.com>
2026-02-20 15:02:16 +0800

    fix: Fixed an error of infinite loop due to single character limitations, so instead of chars we used integers as well as intigrating a verification step to verify if the number input is real or not

--- Projects/mini_projects/basic_calculator.c
+++ Projects/mini_projects/basic_calculator.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 // Declare the functions that will be used here
-void get_values(double *n1, double *n2);
 
+// QoL Functions
+void get_values(double *n1, double *n2);
+void main_menu();
+
+// main functions
 void addNum(double n1, double n2);
 void subtractNum(double n1, double n2);
 void multiplyNum(double num1, double n2);
@@ -10,42 +14,49 @@ void divideNum(double n1, double n2);
 
 int main(void) {
 
-    char choice;
+    int choice;
+    int checker;
     double num1;
     double num2;
-   
-    printf("======= Welcome to Calculator =======\n");
-    printf("\nChoose your operator (+, -, x, /) or press q to exit\n");
-
-    printf("Prompt: ");
-    scanf("%c", &choice);
     
     do {
+        
+        main_menu();
+        printf("Prompt: ");
+        checker = scanf("%d", &choice);
+
+        if (checker != 1) {
+            printf("Error: Invalid input. Please enter a valid number in choices.\n");
+            while (getchar() != '\n');
+            choice = -1;
+            continue;
+        }
+
         switch (choice) {
-            case '+':
+            case 1:
                 get_values(&num1, &num2);
                 addNum(num1, num2);
                 break;
-            case '-':
+            case 2:
                 get_values(&num1, &num2);
                 subtractNum(num1, num2);
                 break;
-            case 'x':
+            case 3:
                 get_values(&num1, &num2);
                 multiplyNum(num1, num2);
                 break;
-            case '/':
+            case 4:
                 get_values(&num1, &num2);
                 divideNum(num1, num2);
                 break;
-            case 'q':
+            case 0:
                 printf("Exiting....");
                 break;
             default:
                 printf("Cannot recognize input.. Try again\n");
                 break;
         }
-    } while (choice != 'q');
+    } while (choice != 0);
 
     return 0;
 }
@@ -56,6 +67,16 @@ void get_values(double* n1, double* n2) {
     printf("Enter the second number: ");
     scanf("%lf", n2);
 }
+
+void main_menu() {
+    printf("\n===== MAIN MENU =====\n");
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+    printf("0. Exit\n");
+}
+
 void addNum(double num1, double num2) {
     double result = num1 + num2;
     printf("The result is: %.2lf\n", result);
